95ffdf52a81e3d773ee36c38545f97ef1b88fccc
nataliep16 <145048985+nataliep16@users.noreply.github.com>
2025-11-14 13:10:28 -0500

    Update Overhead_Pass_Time.m

--- Overhead_Pass_Time.m
+++ Overhead_Pass_Time.m
@@ -2,7 +2,7 @@ clear all;
 clc;
 
 R = 3396.18255; %Mars radius (km)
-h = 100:100:500; %satellite altitude (km) increments 
+h = [1000,1850,4000,5000,13500,15000,15000,15500,17500,19000]; %satellite altitude (km) increments 
 %^^^ if circular and the same then just one #
 ep = 10; %min elevation angle (rad/s) JUST A FILL IN NEED TO CHANGE
 mu = 4.282837*10^4; %Mars' gravitational parameter (km^3/s^2)
@@ -22,3 +22,4 @@ O_omega = abs(omega_sat + omega_mars); %or minus depending on the orbit directio
 T = (2*psi_max)./O_omega %in seconds
 T_m = T./60 %in minutes
 T_h = T_m./60 % in hours
+
