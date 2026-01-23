d6a2b311ddfc51778923085e90faabe50fdc1848
andrewtabor <131301917+andrewtabor@users.noreply.github.com>
2026-01-23 12:12:36 -0500

    Implemented basic feature matching between query and training image

--- Experiments/ORBStuff.py
+++ Experiments/ORBStuff.py
@@ -2,29 +2,48 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 import os 
 
-# Code largely sourced from https://www.youtube.com/watch?v=0sPlnrEMyYk
-# and https://docs.opencv.org/4.x/d1/d89/tutorial_py_orb.html
+# Code largely sourced from https://docs.opencv.org/4.x/d1/d89/tutorial_py_orb.html and https://docs.opencv.org/4.x/dc/dc3/tutorial_py_matcher.html
 
-# i had to make an environment to run it
+# i had to make an environment to run it. need to have it all be in the omni environment at some point
 # conda create -n marsorb python=3.11 numpy<2 matplotlib -y
 # conda activate marsorb
 
-
 def ORB():
     root = os.getcwd()
-    imgPath = os.path.join(root, "mars_1k_color.jpg")
-    img = cv.imread(imgPath, cv.IMREAD_GRAYSCALE)
+    trainImgPath = os.path.join(root, "../mars_1k_color.jpg") 
+    queryImgPath = os.path.join(root, "../CroppedMars.png")
 
+
+    # load images
+    imgTrain = cv.imread(trainImgPath, cv.IMREAD_GRAYSCALE) #This is the perfect entire map
+    imgQuery = cv.imread(queryImgPath, cv.IMREAD_GRAYSCALE) #This is what we are trying to match to the map
+
+    #initiate org
     orb = cv.ORB_create()
-    kp = orb.detect(img,None)
- 
-    # compute the descriptors with ORB
-    kp, des = orb.compute(img, kp)
-    img = cv.drawKeypoints(img, kp, img, color=(0,255,0), flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-    plt.figure()
-    plt.imshow(img)
-    plt.show()
+    # get keypoints and descriptors for each image
+    kpTrain, desTrain = orb.detectAndCompute(imgTrain, None)
+    kpQuery, desQuery = orb.detectAndCompute(imgQuery,None)
+
+    # create BFMatcher (Brute Force Matcher) object
+    bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True) #can add a KNN method to 
+ 
+    # Match descriptors.
+    matches = bf.match(desQuery,desTrain)
+ 
+    # Sort them in the order of their distance.
+    matches = sorted(matches, key = lambda x:x.distance)
+ 
+    # Draw first 10 matches.
+    img3 = cv.drawMatches(imgQuery,kpQuery,imgTrain,kpTrain,matches[:15],None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+ 
+    plt.imshow(img3),plt.show()
+
+    # #visualize keypoints on train image
+    # trainImg = cv.drawKeypoints(trainImg, kpTrain, trainImg, color=(0,255,0), flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    # plt.figure()
+    # plt.imshow(trainImg)
+    # plt.show()
 
 if __name__ == '__main__':
     ORB()
